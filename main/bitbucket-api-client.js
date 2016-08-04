3e0fb51121079920d373abfdf3a383c14ff499df
Michał Stocki <mstocki@ydp.eu>
2016-08-04 09:24:49 +0200

    Recurrent fetching next pages of results from bitbucket

--- main/bitbucket-api-client.js
+++ main/bitbucket-api-client.js
@@ -3,19 +3,49 @@ const createBasicAuthHeader = require('basic-auth-header');
 const moment = require('moment');
 const fetch = require('node-fetch');
 
-function getFilteredPullRequests(repositoryName, filters) {
-	return fetchPullRequests(repositoryName)
-		.then(checkResponseStatus)
-		.then(json => json.values)
+module.exports.getFilteredPullRequests = function(repositoryName, filters) {
+	return fetchFilteredPullRequests(getRequestUrl(repositoryName), filters);
+};
+
+function getRequestUrl(repositoryName) {
+	return `https://api.bitbucket.org/2.0/repositories/ydp/${repositoryName}/pullrequests?state=MERGED&state=OPEN`;
+}
+
+function fetchFilteredPullRequests(apiURL, filters) {
+	return fetchPullRequestsObject(apiURL)
+		.then(pullRequestsObject => loadNextPage(pullRequestsObject, filters))
+		.then(pullRequestsObject => pullRequestsObject.values)
 		.then(pullRequests => filterPullRequests(pullRequests, filters));
 }
 
-function fetchPullRequests(repositoryName) {
-	return fetch(`https://api.bitbucket.org/2.0/repositories/ydp/${repositoryName}/pullrequests?state=MERGED&state=OPEN`, {
+function fetchPullRequestsObject(bitbucketAPIURL) {
+	return fetch(bitbucketAPIURL, {
 		headers: {
 			'Authorization': createBasicAuthHeader(config.bitbucketUser, config.bitbucketPass)
 		}
-	});
+	}).then(checkResponseStatus);
+}
+
+function loadNextPage(pullRequestsObject, filters) {
+	console.log('pullRequestsObject length', pullRequestsObject.values[0].destination.repository.name,
+		pullRequestsObject.values.length);
+	let nextPageData = pullRequestsObject;
+	if (pullRequestsObject.next && canNextPageSatisfyFilter(pullRequestsObject, filters)) {
+		nextPageData = fetchPullRequestsObject(pullRequestsObject.next).then(prObject => {
+			[].unshift.apply(prObject.values, pullRequestsObject.values);
+			return loadNextPage(prObject, filters);
+		});
+	}
+	return nextPageData;
+}
+
+function canNextPageSatisfyFilter(pullRequestObject, filters) {
+	let result = true;
+	const lastResult = pullRequestObject.values[pullRequestObject.values.length - 1];
+	if (filters.period && moment(lastResult.created_on).isAfter(filters.period.to)) {
+		result = false;
+	}
+	return result;
 }
 
 function filterPullRequests(pullRequests, filters) {
@@ -43,5 +73,3 @@ function filterPullRequestsByUser(pullRequests, username) {
 function filterPullRequestsByPeriod(pullRequests, period) {
 	return pullRequests.filter(pullRequest => moment(pullRequest.created_on).isBetween(period.from, period.to));
 }
-
-module.exports.getFilteredPullRequests = getFilteredPullRequests;
