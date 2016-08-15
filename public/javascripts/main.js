b135caaed541ba552a3e17aa299e33a77d5904b4
Michał Stocki <mstocki@ydp.eu>
2016-08-15 19:35:43 +0200

    Pushing file from server to a browser - draft implementation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,9 +4,9 @@ document.getElementById('month').value = local.toJSON().slice(0,7);
 
 var socket = io.connect(document.location.href);
 var form = document.getElementById('report-form');
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
 	event.preventDefault();
-	const data = getFormData();
+	var data = getFormData();
 	sendReportRequest(data);
 }, false);
 
@@ -32,6 +32,16 @@ function sendReportRequest(data) {
 function setStatus(text) {
 	document.getElementById('status').textContent = text;
 }
+
+socket.on('report-file', function(event) {
+	var blob = new Blob([event.buffer], {type: 'text/csv'});
+	var objectURL = URL.createObjectURL(blob);
+
+	var a = document.createElement('a');
+	a.download = event.filename;
+	a.href = objectURL;
+	a.click();
+});
 
 socket.on('drilling-bitbucket-progress', function (data) {
 	setStatus('Zaciąganie danych o pull requestach z Bitbucketa: ' + (data.progress * 100) + '%')
--- socket/socket-reporter.js
+++ socket/socket-reporter.js
@@ -3,6 +3,7 @@
 const PullRequestListProvider = require('../main/pull-request-list-provider');
 const generator = require('../main/report/report-generator');
 const writer = require('../main/write-file');
+const fs = require('fs');
 
 module.exports = function(server) {
 
@@ -15,11 +16,17 @@ module.exports = function(server) {
 				socket.emit('drilling-bitbucket-progress', {progress: progress});
 			});
 			pullRequestListProvider.on('error', (error) => socket.emit('warning', {info: error}));
+			const reportFilename = `${Date.now()}-${data.username}-report.csv`;
+			const reportFilePath = `tmp/${reportFilename}`;
 			pullRequestListProvider.getPullRequestList().then((pullRequests) => {
 				return generator.generateReport(data, pullRequests);
 			}).then((report) => {
 				console.log('Report generated!');
-				return writer.writeFile(`tmp/${Date.now()}-report.csv`, report);
+				return writer.writeFile(reportFilePath, report);
+			}).then(() => {
+				fs.readFile(reportFilePath, function(err, buf){
+					socket.emit('report-file', { buffer: buf, filename: reportFilename });
+				});
 			}).catch((error) => {
 				console.log('Report not generated', error);
 				socket.emit('fail', {info: error});
