16d57382a78d7949321597767526c77c271ab03e
Michał Stocki <mstocki@ydp.eu>
2016-08-18 16:29:12 +0200

    Getting list of repositories for all selected projects

--- main/repository-list-provider.js
+++ main/repository-list-provider.js
@@ -1,5 +1,29 @@
 const apiClient = require('./bitbucket-api-client');
 
 module.exports.getRepositoriesForProjects = function(projectKeys) {
-	return apiClient.getRepositoriesByProject(projectKeys[0]);
+	const repositoryList = [];
+	let lastPromise = Promise.resolve();
+	projectKeys.forEach((projectKey) => {
+		lastPromise = lastPromise.then(() => {
+			return getRepositoriesForProjects(projectKey, repositoryList);
+		}).catch((error) => {
+			console.log(`Error while fetching data from ${projectKey}: ${error}`);
+			return getRepositoriesForProjects(projectKey, repositoryList);
+		});
+	});
+	return lastPromise.then(() => _finish(repositoryList, projectKeys)).catch(() => _finish(repositoryList, projectKeys));
 };
+
+function getRepositoriesForProjects(projectKey, repositoryList) {
+	return apiClient.getRepositoriesByProject(projectKey).then((repositories) => {
+		[].push.apply(repositoryList, repositories);
+	});
+}
+
+function _finish(repositoryList, projectKeys) {
+	if (repositoryList.length > 0) {
+		return repositoryList;
+	} else {
+		return Promise.reject(`No repositories for projects: ${projectKeys.join(', ')}.`);
+	}
+}
